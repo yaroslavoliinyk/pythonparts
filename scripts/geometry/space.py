ac1dfce372390db274a4329803fd4188d09b9c2f
Yaroslav Oliinyk <iYO>
2023-06-29 20:07:45 +0300

    feat: started filling Space class

--- scripts/geometry/space.py
+++ scripts/geometry/space.py
@@ -1,2 +1,11 @@
 from abc import ABC
+from typing import Optional
 
+from .coords import Coords
+
+
+class Space(ABC):
+    # def __init__(
+    #     self, local: Optional[Coords] = None, global_: Optional[Coords] = None
+    # ):
+        pass
