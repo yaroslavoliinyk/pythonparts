b1448a308b5e04fc031021441a25b20f264aca78
Yaroslav Oliinyk <iYO>
2023-06-29 20:02:25 +0300

    feat: implement Coords class

--- scripts/geometry/coords.py
+++ scripts/geometry/coords.py
@@ -1,1 +1,34 @@
-import NemAll_Python_Geometry as AllplanGeo
+from typing import Optional
+
+import NemAll_Python_Geometry as AllplanGeo    # type: ignore
+
+
+class Coords:
+
+    def __init__(self, start_point: Optional[AllplanGeo.Point3D]=None, end_point: Optional[AllplanGeo.Point3D]=None):
+        if start_point is not None:
+            self.__start_point = start_point
+        if end_point is not None:
+            self.__end_point   = end_point
+
+    @property
+    def start_point(self):
+        return self.__start_point
+    
+    @property.setter
+    def start_point(self, p: AllplanGeo.Point3D):
+        self.__start_point = AllplanGeo.Point3D(p)
+    
+    @property
+    def end_point(self):
+        return self.__end_point
+    
+    @property.setter
+    def end_point(self, p: AllplanGeo.Point3D):
+        self.__end_point = AllplanGeo.Point3D(p)
+    
+    def __repr__(self):
+        return f"Coords(start_point={self.start_point!r}, end_point={self.end_point!r})"
+
+    def __str__(self):
+        return repr(self)
