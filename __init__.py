074638c04f74612f1213060d4e1354e33da806fa
Yaroslav Oliinyk <iYO>
2023-07-03 17:09:18 +0300

    feat: remove setup and include a few tests on import

---
+++ __init__.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append("C:\Program Files\Allplan\Allplan\2023\Prg")
+
+from .scripts import geometry
--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,3 +1,23 @@
 import pytest
 
 
+def test_import_pythonparts():
+    try:
+        import pythonparts
+    except ModuleNotFoundError:
+        assert False, "No such module: pythonparts"
+
+
+def test_import_geometry():
+    try:
+        from pythonparts import geometry
+    except ModuleNotFoundError:
+        assert False, "No such module: pythonparts"
+    except ImportError:
+        assert False, "Failed to import geometry from pythonparts"
+    assert True
+
+
+# def test_coords_import():
+#     try:
+#         from pythonparts.
