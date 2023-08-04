c17902eac7ecd2c7b3c53c0c8b6214b3ccc59686
Yaroslav Oliinyk <y.oliyn7@gmail.com>
2023-08-04 20:11:00 +0300

    test commit 2

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from . import geometry
 from . import utils
 from .exceptions import AttributePermissionError
 from .public import create_cuboid, create_cuboid_from_pyp, create_scene
-# for test commit
+# for test commit 2
