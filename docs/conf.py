2d2635c9835dd91c228902aa238e9be88a7082c1
Yaroslav Oliinyk <y.oliyn7@gmail.com>
2023-08-09 18:21:28 +0300

    bug: again try to solve read the docs issue

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ import sys
 import os
 
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../../../'))
 print('SYYYYS', sys.path)
 sys.path.insert(0, os.path.abspath('../../'))
 
