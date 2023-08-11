4b6f25c171b5f8514bd287393c3fe4696d9c7c2f
Yaroslav Oliinyk <y.oliyn7@gmail.com>
2023-08-11 13:50:17 +0300

    bug: try to fix rtd bug with winreg

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 import sys
+import os
 
-from .config import (path_allplan_pp_api,
-                    path_pp_framework,
-                    path_pp_framework_general_sctips,)
+if os.name == "nt":
+    from .config import (path_allplan_pp_api,
+                        path_pp_framework,
+                        path_pp_framework_general_sctips,)
 
 
-sys.path.append(path_allplan_pp_api())
-sys.path.append(path_pp_framework())
-sys.path.append(path_pp_framework_general_sctips())
+    sys.path.append(path_allplan_pp_api())
+    sys.path.append(path_pp_framework())
+    sys.path.append(path_pp_framework_general_sctips())
 
 
 import NemAll_Python_Geometry as AllplanGeo    # type: ignore
