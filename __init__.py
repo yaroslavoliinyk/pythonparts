31d31fb2c17d6599f23aec9725f231b101dcf867
Yaroslav Oliinyk <y.oliyn7@gmail.com>
2023-08-11 13:58:57 +0300

    bug: try to fix rtd bug with winreg

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 if os.name == "nt":
+    print('NT HERE')
     from .config import (path_allplan_pp_api,
                         path_pp_framework,
                         path_pp_framework_general_sctips,)
@@ -10,8 +11,10 @@ if os.name == "nt":
     sys.path.append(path_allplan_pp_api())
     sys.path.append(path_pp_framework())
     sys.path.append(path_pp_framework_general_sctips())
-
-
     import NemAll_Python_Geometry as AllplanGeo    # type: ignore
+else:
+    print('POSIX HERE')
+
+    
 from .src import create_scene, create_cuboid_from_pyp, create_cuboid, geometry
 
