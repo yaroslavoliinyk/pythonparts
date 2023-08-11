d41946043371bc222f70c6f97ed84210999ea1da
Yaroslav Oliinyk <y.oliyn7@gmail.com>
2023-08-11 14:33:39 +0300

    bug: launch dummy version 0.1.6 , because I somehow broke pip installer

--- __init__.py
+++ __init__.py
@@ -1,19 +1,15 @@
 import sys
 import os
 
-try:
+if os.name == "nt":
     from .config import (path_allplan_pp_api,
                         path_pp_framework,
                         path_pp_framework_general_sctips,)
-
 
     sys.path.append(path_allplan_pp_api())
     sys.path.append(path_pp_framework())
     sys.path.append(path_pp_framework_general_sctips())
     import NemAll_Python_Geometry as AllplanGeo    # type: ignore
-except ModuleNotFoundError:
-    print('unix-based system')
 
-    
 from .src import create_scene, create_cuboid_from_pyp, create_cuboid, geometry
 
