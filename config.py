c8916370ecf1f8ca3c958bcfff1192abf1cfa9f2
Yaroslav Oliinyk <y.oliyn7@gmail.com>
2023-08-11 14:08:35 +0300

    bug: try to fix rtd bug with winreg 2

--- config.py
+++ config.py
@@ -75,5 +75,5 @@ try:
         "GeneralScripts" folder within the "pp_framework" directory.
         """
         return path_pp_framework() + r"\GeneralScripts"
-except ModuleNotFoundError:
+except Exception:
     print('unix-base system')
