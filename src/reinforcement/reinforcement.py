28a7e7a8c0c7483e09b76afe5dae019d4a8ab57d
Yaroslav Oliinyk <y.oliyn7@gmail.com>
2023-09-14 16:26:41 +0300

    feat: translate longbars implementation into abstract reinforcement class and

--- src/reinforcement/reinforcement.py
+++ src/reinforcement/reinforcement.py
@@ -1,5 +1,7 @@
 import math
 
+from abc import ABC, abstractmethod
+
 import NemAll_Python_Geometry as AllplanGeo
 import NemAll_Python_Reinforcement as AllplanReinf
 import StdReinfShapeBuilder.LinearBarPlacementBuilder as LinearBarBuilder
@@ -15,39 +17,25 @@ from ..utils import (find_point_on_space,
 
 
 
-class Reinforcement:
-    pass
-
-
-
-class Longbars(Reinforcement):
-    
+class Reinforcement(ABC):
     FRONT_HOOK_CONST = "add_front_hook"
     BACK_HOOK_CONST  = "add_back_hook"
     INTERSECT_CENTER = False                   # If False - Longbars will touch aligned side with 1 point
     id = 0                                     # If True - Longbars will be half diameter inside aligned side
 
     def __init__(self, 
-                 space, 
-                 along_axis, 
-                 split_by_count=False,
-                 split_by_spacing=False,
+                 space,
+                 along_axis,
                  intersect_center=INTERSECT_CENTER,
-                 **properties):
+                 **properties,):
         self.parent_space = space
-        if not split_by_count and not split_by_spacing:                                             
-            raise AllplanGeometryError("Longbars should be split either by count or by spacing")
         self.start_concov = ConcreteCover()
         self.end_concov = ConcreteCover()
         self.along_axis = check_correct_axis(along_axis)
-        if self.along_axis == "z":
-            raise ValueError("Allplan does not allow creation of Longbars along axis Z!")
-        self.split_by_count = split_by_count
-        self.split_by_spacing = split_by_spacing
         self.properties = properties
         self.__class__.id += 1
         self.intersect_center = intersect_center
-        
+
 
     @property
     def start_point(self):
@@ -56,6 +44,92 @@ class Longbars(Reinforcement):
     @property
     def end_point(self):
         return find_point_on_space(self.end_concov, self.parent_space)
+
+    @property
+    def length(self):
+        axis_upper = self.along_axis.upper()
+        return math.fabs(getattr(self.end_point, axis_upper) - getattr(self.start_point, axis_upper))
+
+    @property
+    def width(self):
+        return self.start_point.GetDistance(self.end_placement_point)
+    
+    @abstractmethod
+    def fetch_shape(self, shape_properties): ...
+
+    @abstractmethod
+    def create(self): ...
+
+    def start(self, **concov) -> "Reinforcement":
+        self.start_concov.update(concov)
+        return self
+    
+    def end(self, **concov) -> "Reinforcement":
+        self.end_concov.update(concov)
+        return self
+
+    def _add_front_hook(self):
+        return self.FRONT_HOOK_CONST in self.properties.keys() and self.properties[self.FRONT_HOOK_CONST]
+
+    def _add_back_hook(self):
+        return self.BACK_HOOK_CONST in self.properties.keys() and self.properties[self.BACK_HOOK_CONST]
+
+    def _shift_concrete_covers_to_edge(self):
+        aligned_side = self._find_aligned_side()
+        
+        old_start_value = getattr(self.start_concov, aligned_side)
+        setattr(self.start_concov, aligned_side, old_start_value + self.diameter/2.)
+        old_end_value = getattr(self.end_concov, aligned_side)
+        setattr(self.end_concov, aligned_side, old_end_value + self.diameter/2.)
+
+    def _find_aligned_side(self):
+        identical_sides = self.start_concov.as_sides_set().intersection(self.end_concov.as_sides_set())
+        for side in identical_sides:
+            if self.start_concov.as_dict()[side] is not None and \
+                self.end_concov.as_dict()[side] is not None and \
+                    math.isclose(self.start_concov.as_dict()[side], self.end_concov.as_dict()[side]):
+                return side
+        raise AllplanGeometryError("No aligned side in Longbars!")
+
+    def __getattr__(self, name):
+        if name in self.properties.keys():
+            return self.properties[name]
+        return self.name
+
+
+
+class Longbars(Reinforcement):
+    
+    def __init__(self, 
+                 space, 
+                 along_axis, 
+                 split_by_count=False,
+                 split_by_spacing=False,
+                 intersect_center=Reinforcement.INTERSECT_CENTER,
+                 **properties):
+        super().__init__(space, 
+                         along_axis, 
+                         intersect_center=intersect_center,
+                         **properties)
+        
+        if not split_by_count and not split_by_spacing:                                             
+            raise AllplanGeometryError("Longbars should be split either by count or by spacing")
+        if self.along_axis == "z":
+            raise ValueError("Allplan does not allow creation of Longbars along axis Z!")
+        self.split_by_count = split_by_count
+        self.split_by_spacing = split_by_spacing
+        
+        
+        # self.parent_space = space
+        
+        # self.start_concov = ConcreteCover()
+        # self.end_concov = ConcreteCover()
+        # self.along_axis = check_correct_axis(along_axis)
+        
+        # self.properties = properties
+        # self.__class__.id += 1
+        # self.intersect_center = intersect_center
+        
 
     @property
     def end_placement_point(self):
@@ -72,23 +146,7 @@ class Longbars(Reinforcement):
                                     self.end_point.Y, 
                                     self.start_point.Z)
         
-    @property
-    def length(self):
-        axis_upper = self.along_axis.upper()
-        return math.fabs(getattr(self.end_point, axis_upper) - getattr(self.start_point, axis_upper))
-
-    @property
-    def width(self):
-        return self.start_point.GetDistance(self.end_placement_point)
-    
-
-    def start(self, **concov) -> "Longbars":
-        self.start_concov.update(concov)
-        return self
-    
-    def end(self, **concov) -> "Longbars":
-        self.end_concov.update(concov)
-        return self
+ 
 
     def fetch_shape(self, shape_properties):
         shape_builder = AllplanReinf.ReinforcementShapeBuilder()
@@ -97,11 +155,11 @@ class Longbars(Reinforcement):
         shape_builder.AddPoints(
             [(AllplanGeo.Point3D(), 0), (self.end_point - self.end_placement_point, 0), (0)]
         )
-        if self.__add_front_hook():
+        if self._add_front_hook():
             shape_builder.SetHookStart(self.properties["front_hook_length"],
                                        90.0,
                                        AllplanReinf.HookType.eAngle)
-        if self.__add_back_hook():
+        if self._add_back_hook():
             shape_builder.SetHookEnd(self.properties["front_hook_length"],
                                        90.0,
                                        AllplanReinf.HookType.eAngle)
@@ -109,7 +167,7 @@ class Longbars(Reinforcement):
 
     def create(self):
         if not self.intersect_center:
-            self.__shift_concrete_covers_to_edge()
+            self._shift_concrete_covers_to_edge()
         spacing, count = self.__assign_spacing_count()
         shape_properties = ReinforcementShapeProperties.rebar(
             self.properties["diameter"],
@@ -131,17 +189,6 @@ class Longbars(Reinforcement):
         return rebars
     
 
-    def __getattr__(self, name):
-        if name in self.properties.keys():
-            return self.properties[name]
-        return self.name
-
-    def __add_front_hook(self):
-        return self.FRONT_HOOK_CONST in self.properties.keys() and self.properties[self.FRONT_HOOK_CONST]
-
-    def __add_back_hook(self):
-        return self.BACK_HOOK_CONST in self.properties.keys() and self.properties[self.BACK_HOOK_CONST]
-
     def __assign_spacing_count(self):
         if self.split_by_spacing and self.split_by_count:
             return self.properties["spacing"], self.properties["count"]
@@ -153,19 +200,3 @@ class Longbars(Reinforcement):
             return spacing, self.properties["count"]
         raise AttributePermissionError("Unnown error. Contact Developer. Should not have reached this point.")
     
-    def __shift_concrete_covers_to_edge(self):
-        aligned_side = self.__find_aligned_side()
-        
-        old_start_value = getattr(self.start_concov, aligned_side)
-        setattr(self.start_concov, aligned_side, old_start_value + self.diameter/2.)
-        old_end_value = getattr(self.end_concov, aligned_side)
-        setattr(self.end_concov, aligned_side, old_end_value + self.diameter/2.)
-
-    def __find_aligned_side(self):
-        identical_sides = self.start_concov.as_sides_set().intersection(self.end_concov.as_sides_set())
-        for side in identical_sides:
-            if self.start_concov.as_dict()[side] is not None and \
-                self.end_concov.as_dict()[side] is not None and \
-                    math.isclose(self.start_concov.as_dict()[side], self.end_concov.as_dict()[side]):
-                return side
-        raise AllplanGeometryError("No aligned side in Longbars!")
